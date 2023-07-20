59d228fd86654b74d805ce4a69a44e1d75ab9232
Onaddib <73098897+Onaddib@users.noreply.github.com>
2023-07-21 01:27:40 +0300

    update

---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Add(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- EntityLayer/Concrete/About.cs
+++ EntityLayer/Concrete/About.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    internal class About
+    public class About
     {
+
+
+
+        [Key]
         public int AboutId { get; set; }
 
         public string Title { get; set; }
