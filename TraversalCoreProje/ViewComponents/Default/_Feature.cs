266db1effcffa4744e1dba5db4df23a98838ec90
Onaddib <73098897+Onaddib@users.noreply.github.com>
2023-07-30 01:22:04 +0300

    update

---
+++ TraversalCoreProje/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _Feature : ViewComponent
+    {
+
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+
+
+        public IViewComponentResult Invoke() {
+
+
+          //  var values = featureManager.TGetList();
+         // ViewBag.image1=featureManager.TGetByID
+            return View(); 
+        
+        }
+    }
+}
