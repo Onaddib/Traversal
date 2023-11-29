285e411d5327ee5db2bd4e3400d5b0c461b9a515
Onaddib <73098897+Onaddib@users.noreply.github.com>
2023-11-29 17:11:02 +0300

    update

---
+++ TraversalCoreProje/Models/UserSignInViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Build.Framework;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+
+namespace TraversalCoreProje.Models
+{
+    public class UserSignInViewModel
+    {
+
+
+        [Required(ErrorMessage="Lütfen Kullanıcı Adınızı Giriniz")]
+        public string ?username   { get; set; }
+
+        [Required(ErrorMessage ="Lütfen Şifrenizi Giriniz")]
+        public string ?password { get; set; }
+    }
+}
